2294a6fa137305f3521b61085ac053deaed716f3
Reid Paape <wrpaape@gmail.com>
2015-05-20 17:45:27 -0500

    hard mode v1

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -20,7 +20,6 @@ class Game
   end
 
   def prompt_user
-
     puts center_message("I'm thinking of a number between 1 and 100", " ")
     puts center_message("Number of guesses left: #{NUM_GUESSES - @guess_count}"," ")
     @guess_count > 0 ? (puts center_message("Last guess: #{@last_guess}"," ")) : (puts " ")
@@ -28,7 +27,6 @@ class Game
     puts " "
     print @next_prompt
     guess = gets.chomp.to_i
-
     if RANGE.member?(guess)
       if guess == @last_guess
         @next_prompt = "What a waste.  Next guess? > "
@@ -39,40 +37,58 @@ class Game
       @next_prompt = "Your positive non-zero integer guess that is less than 101? > "
     end
     guess
-
   end
 
   def is_guess_right?(guess)
-
+    is_guess_right = false
     if RANGE.member?(guess) && guess != @last_guess
       case
       when guess > @correct_num
-        @last_guess = guess
-        @hint = "too high"
-        false
+        if guess > @last_guess
+          case @hint
+          when "too high"
+            @hint = "I said TOO HIGH"
+          when "I said TOO HIGH"
+            @hint = "I give up, enjoy"
+          when "I give up, enjoy"
+            @hint
+          else
+            @hint = "too high"
+          end
+        else
+          @hint = "too high"
+        end
       when guess < @correct_num
-        @last_guess = guess
-        @hint = "too low"
-        false
+        if guess < @last_guess
+          case @hint
+          when "too low"
+            @hint = "I said TOO LOW"
+          when "I said TOO LOW"
+            @hint = "I give up, enjoy"
+          when "I give up, enjoy"
+            @hint
+          else
+            @hint = "too low"
+          end
+        else
+          @hint = "too low"
+        end
       else
-        true
+        is_guess_right = true
       end
     else
       @hint = "hope you remembered"
-      false
     end
-
+    @last_guess = guess
+    is_guess_right
   end
 
   def is_game_over?
-
     @guess_count >= NUM_GUESSES - 1 ? true : false
-
   end
 
 
   def center_message(message,pad_char)
-
     width = `tput cols`.chomp.to_i
     padding = width / 2 -  (message.length / 2)
     if message.length.even?
@@ -80,7 +96,6 @@ class Game
     else
       pad_char * padding + message + pad_char * (padding - 1)
     end
-
   end
 
   def display_header
